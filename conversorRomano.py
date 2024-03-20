63787f2464e838866e4f4f995d27127af9432d67
fmoorenoo <fermontemore@gmail.com>
2024-03-20 10:44:42 +0000

    avanzado conversor romano

--- conversorRomano.py
+++ conversorRomano.py
@@ -1,7 +1,13 @@
-class Romano:
-    def __init__(self, num: int) -> None:
-        self.num = num
-        num = 0
+class Romanos:
+    def enteroARomano(numEntero):
+        numeros = [1000, 900, 500, 400,  100,  90,  50,  40,   10,   9,    5,    4,   1]
+        letras =  ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I']
+        romano = ''
+        i = 0
+        while numEntero > 0:
+            for x in range(numEntero // numeros[i]):
+                romano += letras[i]
+                numEntero -= numeros[i]
+            i += 1
+        return romano
 
-    def conversor(self):
-        pass
